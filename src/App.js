d8fa1658efdc4e3d31a0b3c1c403c0cd2c6f8c19
andrenaP <a0993100106@gmail.com>
2025-08-15 09:30:19 +0300

    Add TopMenu.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,63 @@ import Products from "./pages/Products";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Income from "./pages/Income";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+function TopMenu() {
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleString("ru-RU"),
+  );
+  const [sessionCount, setSessionCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString("ru-RU"));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => {
+      console.log("Connected to server");
+    });
+
+    socket.on("sessionCount", (count) => {
+      setSessionCount(count);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="fixed top-0 right-0 p-4 flex items-center space-x-4 text-gray-800">
+      <span>{currentTime}</span>
+      <span>Активные сессии: {sessionCount}</span>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/income" element={<Income />} />
-          <Route path="/groups" element={<Groups />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <>
+      <TopMenu></TopMenu>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/income" element={<Income />} />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Layout({ children }) {
   return (
     <div className="min-h-screen bg-gray-100">
+      top
       <nav className="bg-blue-600 text-white p-4">
         <ul className="flex space-x-4">
           <li>
